71e0ab5f6b0ac6d3294fee25a8110b1faf06a223
JerielJaro <jarojeriel@gmail.com>
2018-12-06 12:47:25 +0800

    tweaks fixes

--- bitonic-sort.c
+++ bitonic-sort.c
@@ -11,13 +11,15 @@
 int main(int argc, char *argv[]){
     MPI_Init(&argc,&argv);
 
-    int *matrix,
+
+
+    int *vector,
         msize, psize,
         i=0,j,k,l,m,n,
 
     //Preprocessing variables
         maxpower=0, mainpower=1,
-        members, sranks,
+        members, issrc, prank,isplus,
 
     //Slave variables
         ownnum, pairnum,
@@ -32,34 +34,41 @@ int main(int argc, char *argv[]){
     MPI_Comm_rank(MPI_COMM_WORLD,&crank);
     MPI_Get_processor_name(cname,&cnamelen);
 
-    printf("%s rank:%d\n", cname, crank);
-/**
+    
     //Root procedures
     if(crank==ROOT){
-        //Fetch matrix size, power size
-        if(argc>=2) msize=strtol(argv[1],NULL,10);
-        else msize=DEF_SIZE;
+        //Fetch vector size, power size
+        msize=(argc>=2)?strtol(argv[1],NULL,10):DEF_SIZE;
         while(++maxpower){
-            if(msize<=pow(2,i)){
-                psize = pow(2,i);
+            printf("maxpower:%d\n",maxpower);
+            if(msize<=pow(2,maxpower)){
+                psize = pow(2,maxpower);
                 break;
             } 
-        }
-        //Fetch whether ascending or descending sort
-        if(argc>=3) ascending=strtol(argv[2],NULL,10);
-        else ascending = 1;
+        }   
 
-        //generate matrix in power size
-        matrix = malloc(psize*sizeof(*matrix));
-        for(i=0;i<psize;i++) matrix[i] = (i<msize) ? rand()%msize+1 : 10000000;
+        //Fetch whether ascending or descending sort
+        ascending=(argc>=3)?strtol(argv[2],NULL,10):1;
+
+        //generate vector in power size
+        vector = malloc(psize*sizeof(*vector));
+        for(i=0;i<psize;i++) vector[i] = (i<msize) ? rand()%msize+1 : INT_MAX;
+
+        for(i=0;i<psize;i++){
+            printf("%d ", vector[i]);
+        }
+        printf("\n\n");
     }
 
-    // Distribute matrix to slaves' ownnum
-    MPI_Scatter(matrix,psize,MPI_INT,&ownnum,1,MPI_INT,ROOT,MPI_COMM_WORLD);
+    // Distribute vector to slaves' ownnum
+    MPI_Scatter(vector,1,MPI_INT,&ownnum,1,MPI_INT,ROOT,MPI_COMM_WORLD);
     // Distribute maxpower to slaves
     MPI_Bcast(&maxpower,1,MPI_INT,ROOT,MPI_COMM_WORLD);
 
-/*******PREPROCESSING************************************************************
+
+    
+
+/*******PREPROCESSING************************************************************/
     
     //Outer Loop for log(p)-1
     mainpower=1;
@@ -67,9 +76,12 @@ int main(int argc, char *argv[]){
         //Inner loop for mainpower
         for(j=mainpower;j>=0;j--){
             members = pow(2,j);
-            sranks = members/2;
+            issrc   = (crank%members<members/2)?1:0;
+            prank   = (issrc)?crank+pow(2,j-1):crank-pow(2,j-1);
+            isplus  = ((!(int)floor(crank/members)%2 && ascending) || ((int)floor(crank/members)%2 && !ascending)) ? 1:0;
+            
             //Determine whether slave is source or destination in pair
-            if(crank%members<sranks){
+            if(issrc){
                 MPI_Send(&ownnum,1,MPI_INT,crank+pow(2,j-1),1,MPI_COMM_WORLD);
                 MPI_Recv(&pairnum,1,MPI_INT,crank+pow(2,j-1),1,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
             } 
@@ -79,10 +91,25 @@ int main(int argc, char *argv[]){
             }
             if(((int)floor(crank/members)%2 && ascending) || ((int)floor(crank/members)%2 && !ascending)) ownnum = (ownnum < pairnum) ? ownnum : pairnum;
             else ownnum = (ownnum < pairnum) ? pairnum : ownnum;
-
+            
+            printf("%s\tcrank:%2d prank:%2d shdplus:%2d isplus:%2d i:%d j%d\n",cname,crank,prank,(int)floor(crank/members)%2,isplus,i,j);
         }
         mainpower++;
     }
-**/
+
+    MPI_Gather(&ownnum, 1, MPI_INT, vector,1,MPI_INT,ROOT,MPI_COMM_WORLD);
+
+    if(crank==ROOT){
+        for(i=0;i<psize;i++){
+            printf("%d ", vector[i]);
+        }
+        printf("\n\n");
+        free(vector);
+    }
+
+/*****SORTING PROPER*************************************************************/
+
+
     MPI_Finalize();
+    return 1;
 }
