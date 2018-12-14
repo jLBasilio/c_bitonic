9ed99f80ca601da2aa4c89e13d9dc65fecf6efa7
jLBasilio <jlcbasilio@gmail.com>
2018-12-14 23:51:55 +0800

    Outer iteration send

--- new_bitonic.c
+++ new_bitonic.c
@@ -7,8 +7,12 @@
 
 #define DEFAULT_SIZE 16
 #define MASTER_NODE 0
+#define TRUE 1
+#define FALSE 0
 
 void printArray(int *, int);
+int getMin(int, int);
+int getMax(int, int);
 
 void printArray(int *arrayToPrint, int arraySize) {
   printf("\n");
@@ -16,6 +20,16 @@ void printArray(int *arrayToPrint, int arraySize) {
   for(i = 0; i < arraySize; i++)
     printf("%d ", arrayToPrint[i]);
   printf("\n");
+}
+
+int getMin(int a, int b) {
+  
+  return (a < b) ? a : b;
+}
+
+int getMax(int a, int b) {
+
+  return (a > b) ? a : b;
 }
 
 int main(int argc, char *argv[]) {
@@ -32,9 +46,16 @@ int main(int argc, char *argv[]) {
       originalSize,                           // From user
       worldSize,                              // Number of nodes
       selfRank,                               // Node rank
+      pairRank,                               // Node pair rank for compare and swap
       selfNameLength,                         // Node name length
       selfElement = -1,                       // The number held by the node
-      i;                                      // Iterator
+      pairElement = -1,                       // The number held by the node
+
+      outerIteration,                         // Iteration on outer loop
+      innerIteration,
+      skipCount,                              // Increases every iteration
+      increasing,                              // Determine whether increasing or no
+      i, j;                                   // Iterator
 
   char selfName[MPI_MAX_PROCESSOR_NAME];      // Node name
 
@@ -59,30 +80,55 @@ int main(int argc, char *argv[]) {
     /* Initialize array */
     listToSort = (int *) malloc(arraySize * sizeof(int));
     for(i = 0; i < arraySize; i++)
-      listToSort[i] = (i >= originalSize) ? INT_MAX : rand() % arraySize + 1;
-    
+      // listToSort[i] = (i >= originalSize) ? INT_MAX : rand() % arraySize + 1;
+      listToSort[i] = (i >= originalSize) ? INT_MAX : i;
   }
 
-  /* SCATTER! :> */
+  /* Broadcast original and array size */
+  MPI_Bcast(&originalSize, 1, MPI_INT, MASTER_NODE, MPI_COMM_WORLD);
+  MPI_Bcast(&arraySize, 1, MPI_INT, MASTER_NODE, MPI_COMM_WORLD);
+
+  /* Scatter all elements to nodes */
   MPI_Scatter(listToSort, 1, MPI_INT, &selfElement, 1, MPI_INT, MASTER_NODE, MPI_COMM_WORLD);
   
+  /* Pre-processing, make list bitonic */
+  /* Outer loop has an iteration of log(size) - 1 */
+  outerIteration = (int) log2(arraySize) - 1; 
+  skipCount = 1;
+  innerIteration = 1;
+  printf("\n================ RANK %d ================ \n", selfRank);
+
+
+
+  for(i = 0; i < outerIteration; i++) {
+    
+    skipCount = (int) pow(2, i);
+
+    for(j = 0; j < innerIteration; j++) {
+        printf("Current i j: %d %d\n", i, j);
+        if(selfRank % 2 == 0) {
+          MPI_Send(&selfElement, 1, MPI_INT, selfRank + skipCount, 0, MPI_COMM_WORLD);
+          MPI_Recv(&pairElement, 1, MPI_INT, selfRank + skipCount, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+          printf("Received from %d: %d\n", selfRank + skipCount, pairElement);
+        }
+
+        else {
+          MPI_Recv(&pairElement, 1, MPI_INT, selfRank - skipCount, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+          printf("Received from %d: %d\n", selfRank - skipCount, pairElement);
+          MPI_Send(&selfElement, 1, MPI_INT, selfRank - skipCount, 0, MPI_COMM_WORLD);
+        }
+    
+    }
+
+    innerIteration++;
+
+  }
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-  printf("Self rank, element: %d  %d\n", selfRank, selfElement);
+  // printf("Self rank, element, original size: %d  %d  %d\n", selfRank, selfElement, originalSize);
   MPI_Finalize();
   return 0;
 }
--- run.sh
+++ run.sh
@@ -1,3 +1,2 @@
 reset
-mpicc -o new_bitonic new_bitonic.c
-mpirun -np 16 ./new_bitonic 16
+mpicc -o new_bitonic new_bitonic.c -lm && mpirun -np 16 ./new_bitonic 16
